25dc549950016b55c550f4c452e10119d7fa2c52
jyoti arya <jyotiarya9058@gmail.com>
2020-06-09 02:31:57 -0500

    npm scripts 1

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+    
+$("#dismis").click(function(){
+      $("#form").modal('hide');
+    });
+    $("#dismisss").click(function(){
+      $("#form").modal('hide');
+    });
+    $("#dsms").click(function(){
+      $("#loginModal").modal('hide');
+    });
+    $("#dsmss").click(function(){
+      $("#loginModal").modal('hide');
+    });
+    $("#logn").click(function(){
+      $("#loginModal").modal('show');
+    });
+    $("#formshow").click(function(){
+      $("#form").modal('show');
+    });
+});
